f60ab03bf535630cedde2f697e5eddcda4de338e
Rustam Musin <PloadyFree@gmail.com>
2017-05-24 00:42:45 +0400

    Added caching to RelationServiceImpl to decrease database workload

--- src/main/kotlin/my/maryvkweb/Utils.kt
+++ src/main/kotlin/my/maryvkweb/Utils.kt
@@ -1,12 +1,14 @@
 package my.maryvkweb
 
+import org.springframework.cache.Cache
 import org.springframework.cache.CacheManager
 import java.io.InputStream
 import java.nio.charset.Charset
 import java.util.logging.Logger
 
 inline fun <reified T> getLogger(): Logger = Logger.getLogger(T::class.java.name)
-inline fun <reified T> CacheManager.getCache() = getCache(T::class.java.name)
-fun InputStream.readTextAndClose(charset: Charset = Charsets.UTF_8): String {
-    return this.bufferedReader(charset).use { it.readText() }
-}
+inline fun <reified T> CacheManager.getCache(): Cache = getCache(T::class.java.name)
+operator fun Cache.set(key: String, value: Any?) = put(key, value)
+
+fun InputStream.readTextAndClose(charset: Charset = Charsets.UTF_8): String
+        = this.bufferedReader(charset).use { it.readText() }
--- src/main/kotlin/my/maryvkweb/service/RelationServiceImpl.kt
+++ src/main/kotlin/my/maryvkweb/service/RelationServiceImpl.kt
@@ -3,8 +3,9 @@ package my.maryvkweb.service
 import my.maryvkweb.domain.Relation
 import my.maryvkweb.domain.RelationChange
 import my.maryvkweb.domain.RelationType
+import my.maryvkweb.getCache
 import my.maryvkweb.repository.RelationRepository
-import org.springframework.cache.Cache
+import my.maryvkweb.set
 import org.springframework.cache.CacheManager
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -15,44 +16,41 @@ import java.time.LocalDateTime
         cacheManager: CacheManager
 ) : RelationService {
 
-//    private val cache: Cache = cacheManager.getCache("relations")
+    private val cache = cacheManager.getCache<RelationServiceImpl>()
 
     override fun findAllFor(userId: Int, relationType: RelationType): List<Int> {
-//        val cacheKey = cacheKey(userId, relationType)
-//        val wrapper = cache.get(cacheKey)
-//        var result = wrapper?.get()
-//        if (result == null) {
-//            result = findAllWithQuery(userId, relationType)
-//            cache.put(cacheKey, result)
-//        }
-//        return result as List<Int>
-        return findAllWithQuery(userId, relationType)
+        return getCachedOrLoad(userId, relationType)
     }
-    private fun findAllWithQuery(userId: Int, relationType: RelationType) = relationRepository
-            .findAllByConnectedIdAndRelationTypeOrderByTargetId(userId, relationType)
-            .map { it.targetId!! }
 
     override fun addRelation(relation: Relation) {
-//        cache.evict(relation.cacheKey())
+        getCachedOrLoad(relation).add(relation.targetId!!)
         relationRepository.saveAndFlush(relation)
         relationChangeService.registerChange(relation.createRelationChange(isAppeared = true))
     }
 
     override fun removeRelation(relation: Relation) {
-//        cache.evict(relation.cacheKey())
-        relationRepository.deleteByConnectedIdAndTargetIdAndRelationType(relation.connectedId!!, relation.targetId!!, relation.relationType!!)
+        getCachedOrLoad(relation).remove(relation.targetId!!)
+        relationRepository.deleteByConnectedIdAndTargetIdAndRelationType(
+                connectedId = relation.connectedId!!,
+                targetId = relation.targetId!!,
+                relationType = relation.relationType!!)
         relationChangeService.registerChange(relation.createRelationChange(isAppeared = false))
     }
 
     override fun addRelations(relations: List<Relation>) {
-//        relations.forEach { cache.evict(it.cacheKey()) }
+        relations.forEach { getCachedOrLoad(it).add(it.targetId!!) }
         relationRepository.saveAll(relations)
         relationChangeService.registerChanges(relations.map { it.createRelationChange(isAppeared = true) })
     }
 
     override fun removeRelations(relations: List<Relation>) {
-//        relations.forEach { cache.evict(it.cacheKey()) }
-        relations.forEach { relationRepository.saveAndFlush(it) }   //todo: fix it
+        relations.forEach { relation ->
+            getCachedOrLoad(relation).remove(relation.targetId!!)
+            relationRepository.deleteByConnectedIdAndTargetIdAndRelationType(//todo: make batch
+                    connectedId = relation.connectedId!!,
+                    targetId = relation.targetId!!,
+                    relationType = relation.relationType!!)
+        }
         relationChangeService.registerChanges(relations.map { it.createRelationChange(isAppeared = false) })
     }
 
@@ -64,6 +62,16 @@ import java.time.LocalDateTime
             isAppeared = isAppeared
     )
 
-    private fun cacheKey(userId: Int, relationType:RelationType) = "$userId ${relationType.id}"
-    private fun Relation.cacheKey() = cacheKey(connectedId!!, relationType!!)
+    private fun getCachedOrLoad(userId: Int, relationType: RelationType): MutableList<Int> {
+        val key = "$userId ${relationType.id}"
+        if (cache[key] == null) {
+            val targets = relationRepository
+                    .findAllByConnectedIdAndRelationTypeOrderByTargetId(userId, relationType)
+                    .map { it.targetId!! }
+            cache[key] = targets
+        }
+        return cache[key].get() as MutableList<Int>
+    }
+
+    private fun getCachedOrLoad(relation: Relation) = getCachedOrLoad(relation.connectedId!!, relation.relationType!!)
 }
