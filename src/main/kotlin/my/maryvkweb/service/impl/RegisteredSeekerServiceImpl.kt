41adf272ea0b7d190b90cd98c42d83611fe54ce6
Rustam Musin <PloadyFree@gmail.com>
2017-05-24 01:52:51 +0400

    Added caching to RegisteredSeekerService

--- src/main/kotlin/my/maryvkweb/service/impl/RegisteredSeekerServiceImpl.kt
+++ src/main/kotlin/my/maryvkweb/service/impl/RegisteredSeekerServiceImpl.kt
@@ -12,17 +12,23 @@ import org.springframework.stereotype.Service
 
     private val log = getLogger<RegisteredSeekerServiceImpl>()
 
+    private val registeredSeekers: MutableList<RegisteredSeeker> by lazy { ArrayList<RegisteredSeeker>() }
+
     override fun register(connectedId: Int) {
         val seeker = RegisteredSeeker(connectedId = connectedId)
         registeredSeekerRepository.saveAndFlush(seeker)
+        registeredSeekers.add(seeker)
+
         log.info("Registered new seeker: " + seeker)
     }
 
     override fun unregister(connectedId: Int) {
-        val seeker = RegisteredSeeker(connectedId = connectedId)
+        registeredSeekers.removeIf { it.connectedId == connectedId }
         registeredSeekerRepository.deleteByConnectedId(connectedId)
+
+        val seeker = RegisteredSeeker(connectedId = connectedId)
         log.info("Unregistered seeker: " + seeker)
     }
 
-    override fun findAll(): List<RegisteredSeeker> = registeredSeekerRepository.findAll()
+    override fun findAll(): List<RegisteredSeeker> = registeredSeekers
 }
